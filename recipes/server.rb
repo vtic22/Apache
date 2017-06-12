2f943d8b8ff12cc2801050187fc91c4a22c9ba2d
victor <vtic22@gmail.com>
2017-06-12 13:17:36 +0200

    Añado Version 0.2.1 Uso de Template para la web index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -6,11 +6,8 @@
 
 package "httpd"
 
-file "/var/www/html/index.html" do
-content "<h1> Hello, world!<h1>
-<h2>ipaddress: #{node['ipaddress']}</h2>
-<h2>hostname: #{node['hostname']}<h2>
-"
+template "/var/www/html/index.html" do
+  source "index.html.erb"
 end
 
 service "httpd" do
